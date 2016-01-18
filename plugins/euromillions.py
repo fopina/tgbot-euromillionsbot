0daf3098b67dced8ecf7ffecc8e96200aafe4080
Filipe Pina <fopina@gmail.com>
2016-01-18 18:06:03 +0000

    cron update/populate actions and tests

--- plugins/euromillions.py
+++ plugins/euromillions.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from tgbot.pluginbase import TGPluginBase, TGCommandBase
 from tgbot.tgbot import ChatAction
+import requests
 
 
 class EuromillionsPlugin(TGPluginBase):
@@ -12,3 +13,34 @@ class EuromillionsPlugin(TGPluginBase):
     def last(self, message, text):
         self.bot.send_chat_action(message.chat.id, ChatAction.TEXT)
         self.bot.send_message(message.chat.id, 'test')
+
+    def cron_go(self, action, *args):
+        if action == 'millions.populate':
+            return self.cron_populate()
+        elif action == 'millions.update':
+            return self.cron_update()
+
+    def cron_populate(self):
+        import time
+        for year in xrange(2004, time.gmtime().tm_year + 1):
+            print 'Year:', year
+            r = requests.get('https://nunofcguerreiro.com/api-euromillions-json', params={'result': year})
+            for d in r.json()['drawns']:
+                dt = d['date']
+                del(d['date'])
+                self.save_data('results', key2=dt, obj=d)
+        r = requests.get('https://nunofcguerreiro.com/api-euromillions-json')
+        d = r.json()['drawns'][0]
+        self.save_data('results', 'latest', obj=d)
+
+    def cron_update(self):
+        import time
+
+        # do nothing if not Tuesday nor Friday
+        wday = time.gmtime().tm_wday
+        if wday not in [1, 4]:
+            return
+
+        r = requests.get('https://nunofcguerreiro.com/api-euromillions-json')
+        d = r.json()['drawns'][0]
+        self.save_data('results', 'latest', obj=d)
---
+++ tests/test_euromillions.py
@@ -0,0 +1,150 @@
+# coding=utf-8
+from tgbot import plugintest
+from tgbot.botapi import Update
+from plugins.euromillions import EuromillionsPlugin
+
+from requests.packages import urllib3
+urllib3.disable_warnings()
+
+
+class EuromillionsPluginTest(plugintest.PluginTestCase):
+    def setUp(self):
+        self.plugin = EuromillionsPlugin()
+        self.bot = self.fake_bot('', plugins=[self.plugin])
+        self.received_id = 1
+
+    def test_last(self):
+        self.receive_message('/last')
+        self.assertReplied(self.bot, 'test')
+
+    def test_cron_populate(self):
+        import mock
+        import time
+
+        def fake_get(*args, **kwargs):
+            if 'params' in kwargs:
+                year = kwargs['params']['result']
+            else:
+                year = time.gmtime().tm_year
+
+            r = type('Test', (object,), {})()
+            r.json = lambda: {
+                "drawns": [
+                    {
+                        "date": "%d-01-01" % year,
+                        "numbers": "10 19 38 43 46",
+                        "stars": "1 11 %d" % year
+                    }
+                ]
+            }
+            return r
+
+        with mock.patch(
+                'time.gmtime',
+                return_value=time.struct_time((2005, 1, 1, 20, 50, 36, 0, 18, 0))
+        ):
+            with mock.patch('requests.get', fake_get):
+                self.plugin.cron_go('millions.populate')
+
+        self.assertEqual(
+            list(self.plugin.iter_data_key_keys('results')),
+            [u'2004-01-01', u'2005-01-01', u'latest']
+        )
+        self.assertEqual(
+            self.plugin.read_data('results', 'latest'),
+            {
+                "date": "2005-01-01",
+                "numbers": "10 19 38 43 46",
+                "stars": "1 11 2005"
+            }
+        )
+        self.assertEqual(
+            self.plugin.read_data('results', '2004-01-01'),
+            {
+                "numbers": "10 19 38 43 46",
+                "stars": "1 11 2004"
+            }
+        )
+        self.assertEqual(
+            self.plugin.read_data('results', '2005-01-01'),
+            {
+                "numbers": "10 19 38 43 46",
+                "stars": "1 11 2005"
+            }
+        )
+
+    def test_cron_update(self):
+        import mock
+        import time
+
+        def fake_get(*args, **kwargs):
+            r = type('Test', (object,), {})()
+            r.json = lambda: {
+                "drawns": [
+                    {
+                        "date": "2005-01-01",
+                        "numbers": "10 19 38 43 46",
+                        "stars": "1 11"
+                    }
+                ]
+            }
+            return r
+
+        # update on Monday (should do nothing)
+        with mock.patch(
+                'time.gmtime',
+                return_value=time.struct_time((2005, 1, 1, 20, 50, 36, 0, 18, 0))
+        ):
+            with mock.patch('requests.get', fake_get):
+                self.plugin.cron_go('millions.update')
+
+        self.assertEqual(
+            list(self.plugin.iter_data_key_keys('results')),
+            []
+        )
+
+        # update on Tuesday (should update)
+        with mock.patch(
+                'time.gmtime',
+                return_value=time.struct_time((2005, 1, 1, 20, 50, 36, 1, 18, 0))
+        ):
+            with mock.patch('requests.get', fake_get):
+                self.plugin.cron_go('millions.update')
+
+        self.assertEqual(
+            list(self.plugin.iter_data_key_keys('results')),
+            [u'latest']
+        )
+        self.assertEqual(
+            self.plugin.read_data('results', 'latest'),
+            {
+                "date": "2005-01-01",
+                "numbers": "10 19 38 43 46",
+                "stars": "1 11"
+            }
+        )
+
+    def receive_message(self, text, sender=None, chat=None):
+        if sender is None:
+            sender = {
+                'id': 1,
+                'first_name': 'John',
+                'last_name': 'Doe',
+            }
+
+        if chat is None:
+            chat = sender
+
+        self.bot.process_update(
+            Update.from_dict({
+                'update_id': self.received_id,
+                'message': {
+                    'message_id': self.received_id,
+                    'text': text,
+                    'chat': chat,
+                    'from': sender,
+                }
+            })
+        )
+
+        self.received_id += 1
